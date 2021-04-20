d627403fb0c6bdc6a0b3024035fbed41824766c9
mor19034 <68123745+mor19034@users.noreply.github.com>
2021-04-20 11:18:12 -0600

    si hombre
    ya está

---
+++ Labs_micros_C.X/Lab08.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Lab08.c
+ * Author: pablo
+ *
+ * Created on 20 de abril de 2021, 11:17
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
